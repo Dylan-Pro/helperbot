485edfbffde81e708fa4d0a03f68eedf75299a3f
unknown <fabrizioivan36@gmail.com>
2021-02-19 17:38:45 -0600

    regenerate token

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js'); 
 const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'], fetchAllMembers: true });
 client.config = require('./config.json');
 const fs = require("fs");
