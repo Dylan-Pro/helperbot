82f07df3a051d31fb029f02386b45bdfe0346da5
unknown <fabrizioivan36@gmail.com>
2021-02-20 14:05:55 -0600

    Added nuke command v2

--- commands/moderation/nuke.js
+++ commands/moderation/nuke.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports = {
     name: "nuke",
     aliases: [],
-    description: "",
+    description: "Clone a server channel",
     category: "Moderation",
     usage: "nuke",
     cooldown: 3,
@@ -11,21 +11,21 @@ module.exports = {
 
         if (!message.member.permissions.has("MANAGE_CHANNELS")) return message.chhanel.send("<:HBminus:783351288515657728> | You don't have `MANAGE_CHANNELS` permission!");
         if (!message.guild.me.permissions.has("MANAGE_CHANNELS")) return message.chhanel.send("<:HBminus:783351288515657728> | I need `MANAGE_CHANNELS` permission!");
-         try {
-        message.channel.clone().then((ch) => {
-            ch.setParent(message.channel.parent.id);
-            ch.setPosition(message.channel.position);
-            ch.setTopic(message.channel.topic);
-            message.channel.delete();
+        try {
+            message.channel.clone().then((ch) => {
+                ch.setParent(message.channel.parent.id);
+                ch.setPosition(message.channel.position);
+                ch.setTopic(message.channel.topic);
+                message.channel.delete();
 
-            const embedChNuked = new Discord.MessageEmbed()
-                .setDescription(`<#${ch.id}> was nuked! <:HBtrash:783351287504437280>`)
-                .setColor("#0FFF00")
-            ch.send(embedChNuked);
+                const embedChNuked = new Discord.MessageEmbed()
+                    .setDescription(`<#${ch.id}> was nuked! <:HBtrash:783351287504437280>`)
+                    .setColor("#0FFF00")
+                ch.send(embedChNuked);
 
-        })
-    } catch (e) {
-        message.channel.send(`<:HBwarning:783351287944970251> | **${e.name}:** ${e.message}`)
-    }
+            })
+        } catch (e) {
+            message.channel.send(`<:HBwarning:783351287944970251> | **${e.name}:** ${e.message}`);
+        }
     }
 }
