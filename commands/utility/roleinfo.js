4dd1ec3cbe651918dc9d0eb4c252724899903fb9
unknown <fabrizioivan36@gmail.com>
2021-02-23 17:36:05 -0600

    Roleinfo rework

--- commands/utility/roleinfo.js
+++ commands/utility/roleinfo.js
@@ -12,31 +12,42 @@ module.exports = {
 
     const role = message.mentions.roles.first() || message.guild.roles.cache.get(args[0]) || message.guild.roles.cache.find(e => e.name.match(new RegExp(`${args[0]}`, 'gi')));
 
-    if(!role) return message.channel.send("<:HBminus:783351288515657728> | Specify a role")
-    function Markdown(str)  {
-        return `\`\`\`\n${str}\n\`\`\``;
-        };
-    
-    
+
+    if (!role) return message.channel.send("<:HBminus:783351288515657728> | You must mention a role!");
+    function Markdown(str) {
+      return `\`\`\`\n${str}\n\`\`\``;
+    };
+
+    function checkDays(date) {
+      let now = new Date();
+      let diff = now.getTime() - date.getTime();
+      let days = Math.floor(diff / 86400000);
+      return days + (days == 1 ? " day" : " days") + " ago";
+    };
+
+    const membersxd = role.members.filter((x) => x.id !== message.guild.id).map((x) => `${x}`)
+    const listaRoles = membersxd.length > 10 ? `${membersxd.slice(0, 10).join(' | ')} and **${membersxd.length - 10}** more members` : membersxd.join(' | ');
+
     const EmbedRolInfo = new Discord.MessageEmbed()
-    .setThumbnail(message.guild.iconURL({ dynamic: true }))
-    .setTitle("**<:HBfolder:783351287868817448> Role information**")
-    .setDescription("This is the information")
-    .setColor("RANDOM")
-    .addField("<:HBinformation:783351288062672896> **Name**", ` ${role.name}`) 
-    .addField("<:HBfile:783351289224101928>  **ID**", ` ${role.id}`) 
-    .addField("<:HBrefresh:783351288292442183> **Created At**", humanizeDuration(Date.now() - new Date(role.createdAt).getTime(), { largest: 2, conjunction: " and ", round: true }))
-    .addField("<:HBuser:783351289114918973> **Members with the Role**", ` ${role.members.size}`) 
-    .addField("<:HBshield:783351288313937991>  **Position**", ` ${role.rawPosition}`) 
-    .addField("<:HBflag:783351287709171752> **HexColor**", ` ${role.hexColor}`) 
-    .addField("<:HBfunnel:783351288670584862>  **Mentionable?**", role.mentionable ? 'Yes' : 'Not' ) 
-    .addField("<:HBsearch:783351288149835857> **Separated?**", role.hoist ? 'Yes' : 'Not') 
-    .addField("<:HBsettings:783351288536629268> **Managed by the system?**", role.managed ? 'Yes' : 'Not')
-    .addField("<:HBprohibition:783351287868817419> **Permissions**", Markdown(role.permissions.toArray().join(', ')))
-    .setFooter(" Helper Bot | Utility System", client.user.displayAvatarURL()) 
-  
-          
-          
+      .setThumbnail(message.guild.iconURL({ dynamic: true }))
+      .setColor(role.hexColor || "BLUE")
+      .addField("<:HBbarchart:783351287676665917> **Role Information:**", [
+        `**Name:** ${role.name}`,
+        `**Mention:** <@&${role.id}>`,
+        `**ID:** ${role.id}`,
+        `**Created At:** ${role.createdAt.toUTCString().substr(0, 16)} (${checkDays(role.createdAt)})`,
+        `**Mentionable:** ${role.mentionable ? 'Yes' : 'No'}`,
+        `**Separated:** ${role.hoist ? 'Yes' : 'No'}`,
+        `**Managed by the system:** ${role.managed ? 'Yes' : 'No'}`,
+        `**HexColor:** ${role.hexColor}`,
+      ])
+      .addField("<:HBuser:783351289114918973> **Role Members:**", [
+        `**Members with the Role:** ${role.members.size}`,
+        `**Members:**\n${listaRoles || "Has no members"}`
+      ])
+      .addField('<:HBshield:783351288313937991> **Role Permissions:**', Markdown(role.permissions.toArray().join(', ')))
+      .setFooter(" Helper Bot | Utility System", client.user.displayAvatarURL())
+
     message.channel.send(EmbedRolInfo);
 
   }
